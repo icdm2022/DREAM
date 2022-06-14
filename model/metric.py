b57cc3accd725033ad8e79df670bb6c5d1ccd9ec
icdm2022 <107287907+icdm2022@users.noreply.github.com>
2022-06-14 01:28:18 -0400

    Create metric.py

---
+++ model/metric.py
@@ -0,0 +1,12 @@
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
+
+
+
+def accuracy(output, target):
+    return accuracy_score(target, output)
+
+def confusion(output, target):
+    return confusion_matrix(target,output )
+
+def f1(output, target):
+    return f1_score(target,output, average='macro')
